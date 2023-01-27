49cd0932d0477d5514450e9ef143426b0a6b7f01
Julien Palmas <julien@crx.io>
2023-01-27 12:26:13 +0100

    🚨 Remove stale EmberObject import

--- app/components/test/foo-bar.ts
+++ app/components/test/foo-bar.ts
@@ -1,24 +1,24 @@
-import EmberObject, { action } from '@ember/object';
+import { action } from '@ember/object';
 import Component from '@glimmer/component';
 import { EmptyObject } from '@glimmer/component/-private/component';
 import { tracked } from '@glimmer/tracking';
 
 export default class FooBar extends Component {
-    @tracked
-    files: string[];
+  @tracked
+  files: string[];
 
-    constructor(owner: unknown, args: EmptyObject) {
-      super(owner, args);
-        const files: string[] = [];
-        for (let i = 0; i < 10; i++) {
-            files.push(`file-${i}`);
-        }
-        this.files = files;
+  constructor(owner: unknown, args: EmptyObject) {
+    super(owner, args);
+    const files: string[] = [];
+    for (let i = 0; i < 10; i++) {
+      files.push(`file-${i}`);
     }
+    this.files = files;
+  }
 
-    @action
-    addFile(): void {
-        this.files.push(`file-${this.files.length}`);
-        this.files = this.files;
-    }
+  @action
+  addFile(): void {
+    this.files.push(`file-${this.files.length}`);
+    this.files = this.files;
+  }
 }
